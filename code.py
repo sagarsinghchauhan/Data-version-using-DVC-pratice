366e532c3fe85632bbff0158aa251e1e1a073e2f
sagarsinghchauhan <singhsagarc@gmail.com>
2025-09-26 20:56:01 +0530

    now i created code.py for code and data folder save my ouput of code.py file

---
+++ code.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import os 
+
+data = {"Name" : ["sagar","vineet","lalit"],
+        "age" : [18,34,34]
+
+}
+
+df = pd.DataFrame(data)
+# print(df)
+
+data_dir = 'Data'
+
+os.makedirs(data_dir,exist_ok =True)
+
+file_path = os.path.join(data_dir,"sample.csv")
+#define file path 
+df.to_csv(file_path,index = False)
+
+
+print(f"file_path : {file_path}")
